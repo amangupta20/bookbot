05a003bc6243c6e60c989d9a3e3f81796550e113
amangupta20 <58002413+amangupta20@users.noreply.github.com>
2025-08-07 20:25:37 +0530

    args for book input

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 from stats import get_num_words, get_num_char, dict_sort
 
 
@@ -8,17 +9,23 @@ def get_books_text(path_to_tile):
 
 
 # print(get_num_char(get_books_text("books/frankenstein.txt")))
-bookloc = "books/frankenstein.txt"
+if len(sys.argv) != 2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
+
+
+bookloc = sys.argv[1]
+
 print("============ BOOKBOT ============")
-print("Analyzing book found at books/frankenstein.txt...")
+print("Analyzing book found at ", bookloc, "...", sep="")
 print("----------- Word Count ----------")
 print(
     "Found",
-    get_num_words(get_books_text("books/frankenstein.txt")),
+    get_num_words(get_books_text(bookloc)),
     "total words",
 )
 print("--------- Character Count -------")
-sorteddict = dict_sort(get_num_char(get_books_text("books/frankenstein.txt")))
+sorteddict = dict_sort(get_num_char(get_books_text(bookloc)))
 for item in sorteddict:
     if item["char"].isalpha():
         print(item["char"], ": ", item["num"], sep="")
